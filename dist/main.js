c835163fa80007a025bd7ebf52f3721c416182c2
telmotini77 <tjtinitana@sudamericano.edu.ec>
2026-06-05 00:51:25 -0500

    2

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
     const port = process.env.PORT || 3000;
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
     console.log(`NestJS server is running on: http://localhost:${port}`);
 }
 bootstrap().catch((err) => {
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`NestJS server is running on: http://localhost:${port}`);
 }
 bootstrap().catch((err: unknown) => {
