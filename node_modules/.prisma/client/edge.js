c1a5ed3423ea7035be76e6e378d9e95be6d9b0ca
telmotini77 <tinitanatelmo@gmail.com>
2026-07-07 21:26:47 -0500

    6

--- node_modules/.prisma/client/edge.js
+++ node_modules/.prisma/client/edge.js
@@ -317,7 +317,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- node_modules/.prisma/client/index.js
+++ node_modules/.prisma/client/index.js
@@ -318,7 +318,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
